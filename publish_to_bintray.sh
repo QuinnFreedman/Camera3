9384326b6013ddedb2202b1726cc22df077ec2fb
Quinn <quinnfreedman@gmail.com>
2018-08-18 16:30:18 -0700

    0.3.2

--- publish_to_bintray.sh
+++ publish_to_bintray.sh
@@ -1,3 +1,4 @@
 ./gradlew :camera3:clean && \
 ./gradlew :camera3:assembleRelease && \
-./gradlew :camera3:bintrayUpload
+./gradlew :camera3:bintrayUpload && \
+python -mwebbrowser https://bintray.com/login?forwardedFrom=%2Fquinnfreedman%2Fcom.avalancheevantage.android
